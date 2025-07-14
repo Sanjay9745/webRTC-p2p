ed67a790e53f787cbbd82af47d2589c11fc60f3e
Sanjay9745 <sanjysanthosh919@gmail.com>
2025-07-14 20:18:15 +0530

    code added

--- server.js
+++ server.js
@@ -8,21 +8,27 @@ const WebSocket = require('ws');
 
 // Create an HTTP server to serve index.html
 const server = http.createServer((req, res) => {
-    let filePath = path.join(__dirname, 'index.html');
-    if (req.url !== '/' && req.url !== '/index.html') {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('404 Not Found');
+    // Serve index.html for /, /index.html, or /ROOMCODE (6 alphanumeric, case-insensitive)
+    const roomCodeMatch = /^\/([a-zA-Z0-9]{6})$/.exec(req.url);
+    if (
+        req.url === '/' ||
+        req.url === '/index.html' ||
+        roomCodeMatch
+    ) {
+        let filePath = path.join(__dirname, 'index.html');
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('500 Internal Server Error');
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
         return;
     }
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('500 Internal Server Error');
-            return;
-        }
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-    });
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
 });
 
 // Create a new WebSocket server on top of the HTTP server
